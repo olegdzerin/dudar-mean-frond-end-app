3014b565ff2bbbba8b8c3fac3f37d45cf122ff15
olegdzerin <oleg.dz2171@gmail.com>
2021-07-04 15:18:35 +0300

    last commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AuthService } from './auth.service';
 
-const routes: Routes = [];
+import { AuthComponent } from './auth/auth.component';
+import { CanActivateTeam } from './canActivate/can-activate-team.service';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import {HomeComponent} from './home/home.component'
+import { RegComponent } from './reg/reg.component';
+// import { CanActivateTeam } from './canActivate/can-activate-team.service';
+
+const routes: Routes = [
+  {path: '', component: HomeComponent},
+  {path: 'reg', component: RegComponent},
+  {path: 'auth', component: AuthComponent},
+  {path: 'dashboard',canActivate:[CanActivateTeam], component: DashboardComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,46 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { AuthComponent } from './auth/auth.component';
+import { RegComponent } from './reg/reg.component';
+import { HomeComponent } from './home/home.component';
+import { FooterComponent } from './footer/footer.component';
+import { FormsModule } from '@angular/forms';
+import { CheckFormService } from './check-form.service';
+import { FlashMessagesModule, FlashMessagesService } from 'angular2-flash-messages';
+import { AuthService } from './auth.service';
+import { HttpClientModule } from '@angular/common/http';
+
+import { UserTokenService } from './canActivate/user-token.service';
+import { PermissionsService } from './canActivate/permissions.service';
+import { CanActivateTeam } from './canActivate/can-activate-team.service';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    DashboardComponent,
+    AuthComponent,
+    RegComponent,
+    HomeComponent,
+    FooterComponent
   ],
   imports: [
+    FormsModule,
+    HttpClientModule,
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FlashMessagesModule,
   ],
-  providers: [],
+  providers: [CheckFormService,
+     AuthService,
+     CanActivateTeam,
+     UserTokenService,
+     PermissionsService,
+     FlashMessagesService
+],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
